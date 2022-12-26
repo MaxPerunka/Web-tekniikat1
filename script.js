72234e67d067e41a4e723f3f1cdfb828f754fc73
MaxPerunka <n2pema00@students.oamk.fi>
2022-12-26 20:09:59 +0200

    added functionality and debug

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const text1 = document.getElementById("fname");
+const text2 = document.getElementById("note");
+const button1 = document.getElementById("button");
+const out1 = document.getElementById("output1");
+
+function display() {
+    out1.innerHTML = text1.value;
+}
+
+button1.addEventListener('click',display);
