fde3af0a6992a0084ca755f4d7049abd3d1d9f10
MaxPerunka <n2pema00@students.oamk.fi>
2023-01-04 23:03:48 +0200

    final changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var time = new Date();
 var date = time.getFullYear()+'.'+(time.getMonth()+1)+'.'+time.getDate();
 
 function display() {
-    out1.innerHTML += "<strong>" + date + " " + "(" + text1.value + ")" + "</strong>" + "<br>" + text2.value ;
+    out1.innerHTML += "<p>" + "<strong>" + date + " " + "(" + text1.value + ")" + "</strong>" + "<br>" + text2.value + "</p>"  ;
 }
 
 
