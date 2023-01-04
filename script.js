ea6de91d01a23e4d657306263a3259acedc5b43d
MaxPerunka <n2pema00@students.oamk.fi>
2023-01-04 22:21:47 +0200

    changes

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const text1 = document.getElementById("fname");
 const text2 = document.getElementById("note");
 const button1 = document.getElementById("button");
 const out1 = document.getElementById("output1");
+
 var time = new Date();
 var date = time.getFullYear()+'.'+(time.getMonth()+1)+'.'+time.getDate();
 
 function display() {
-    out1.innerHTML = date + " " + "<strong>" + "(" + text1.value + ")" + "</strong>" + "<br>" + text2.value;
+    out1.innerHTML += "<strong>" + date + " " + "(" + text1.value + ")" + "</strong>" + "<br>" + text2.value ;
 }
+
+
 
 button1.addEventListener('click',display);
